c20ec4eb60ef98a54552587dc61e5db00d9f5898
fumeco <fabio_hv_silva@estudante.sc.senai.br>
2019-08-23 15:08:47 -0300

    conexão com banco de dados

---
+++ configDB.php
@@ -0,0 +1,16 @@
+<?php 
+//configDB.php
+
+//Dados para escolher do Database (DB)
+$dbhost = "localhost";
+$dbuser = "root";   //Usuário Raiz (Rute)
+$dbpass = "";
+$dbname = "sistemaDeLogin";
+
+//Conexão com o banco de dados
+$conecta = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if($conecta->connect_error){
+    die("Não foi possivel conectar ao Banco de Dados: " . $conecta->connect_error);
+}else{
+    echo "<h1>conectou no BD Manowwwww!</h1>";
+}
