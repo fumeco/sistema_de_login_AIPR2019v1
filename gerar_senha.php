16565e762dffc181a518306457a2c46d0c1fbdbd
Fabio Henrique <fabio_hv_silva@estudante.sc.senai.br>
2019-09-19 15:04:24 -0300

    Geração de Token

--- gerar_senha.php
+++ gerar_senha.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <!-- Required meta tags -->
@@ -16,7 +16,7 @@
     <main class="container">
         <section class="row justify-content-center">
             <div class="col-lg-5 mt-5">
-                <h3 class="text-center bg-dark text-light">
+                <h3 class="text-center bg-dark text-light p-2 rounded">
                     Crie uma nova senha
                 </h3>
                 <h4 class="text-center">
@@ -25,34 +25,34 @@
                 <form action="" method="post">
                     <div class="form-group">
                         <label for="senha">
-                            Nova senha
+                            Nova Senha
                         </label>
                         <input type="password" name="senha" id="senha" class="form-control" placeholder="Nova Senha" required>
-                        <label for="senha">
-                            Confirmar senha
+                    </div>
+                    <div class="form-group">
+                        <label for="csenha">
+                            Confirme a Senha
                         </label>
                         <input type="password" name="csenha" id="csenha" class="form-control" placeholder="Nova Senha" required>
+                    </div>
                     <div class="form-group">
-                        <input type="submit"
-                        value="Criar Nova Senha"
-                        name="criar"
-                        class="btn btn-block"
-                        style="background: purple;
-                        color: white;
-                        font-weight: bolder;
-                        padding: 10px,
-                        font-size: 22px;">
-                        </div>
-                    </form>
-                </div>
-            </section>
-        </main>
+                        <input type="submit" value=":: Criar nova senha ::" name="criar" class="btn btn-block" style="background: purple; 
+                                color: white; 
+                                font-weight: bolder;
+                                padding: 10px;
+                                font-size: 22px;
+                                box-shadow: 3px 3px 3px gray;">
+                    </div>
+                </form>
+            </div>
+        </section>
+    </main>
 
-                        <!-- Optional JavaScript -->
-                        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-                        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
